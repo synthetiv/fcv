9acf8c36ff89f39fee168020827cea8c3eb6edf1
Kenji Garland <kenjigarland@gmail.com>
2021-09-18 22:36:26 +0700

    Add & remove notes with k3 and k2

--- tropy.lua
+++ tropy.lua
@@ -3,10 +3,23 @@
 playhead_x = 1
 play_clock = nil
 
+notes = {}
+erasing = false
+
 function tick()
+	local prev_playhead_x = playhead_x
 	playhead_x = playhead_x + 1
 	if playhead_x > 128 then
 		playhead_x = playhead_x - 128
+	end
+	for i, note in ipairs(notes) do
+		if note.x > prev_playhead_x and note.x <= playhead_x then
+			if erasing then
+				table.remove(notes, i)
+			else
+				print 'ping'
+			end
+		end
 	end
 end
 
@@ -28,6 +41,22 @@ function redraw()
 	screen.line_width(1)
 	screen.level(4)
 	screen.stroke()
+	
+	for i, note in ipairs(notes) do
+		screen.circle(note.x, 32, 1)
+		screen.level(6)
+		screen.fill()
+	end
 
 	screen.update()
+end
+
+function key(n, z)
+	if n == 2 then
+		erasing = z == 1
+	elseif n == 3 then
+		if z == 1 then
+			table.insert(notes, { x = playhead_x })
+		end
+	end
 end
