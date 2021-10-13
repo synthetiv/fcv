6d4211a9395361f525d1ce5301ddfd39a53cd1fb
Kenji Garland <kenjigarland@gmail.com>
2021-10-14 05:44:21 +0700

    Notes -> Nodes

--- fcv.lua
+++ fcv.lua
@@ -19,11 +19,11 @@ play_x = 0
 m = nil
 g = nil
 
-notes = {}
+nodes = {}
 erasing = false
 
-function sort_notes()
-	table.sort(notes, function(a, b)
+function sort_nodes()
+	table.sort(nodes, function(a, b)
 		return a.home < b.home
 	end)
 end
@@ -52,23 +52,23 @@ end
 -- 7.7227328302841 7.6666666666662 57 = 8  =  8 = 32/21
 -- 7.9339618998232 7.7916666666666 49 = 0  =  0 =  1/1
 
-function notes_where(p)
+function nodes_where(p)
 	local selection = {}
-	for i, note in ipairs(notes) do
-		if p(note) then
-			table.insert(selection, note)
+	for i, node in ipairs(nodes) do
+		if p(node) then
+			table.insert(selection, node)
 		end
 	end
 	return selection
 end
 
 function do_where(p, a)
-	local selection = notes_where(p)
-	for i, note in ipairs(selection) do
-		a(note)
-		-- clean up, if needed (like if we just added a random offset to all notes)
-		note.x = note.x % width
-		note.home = note.home % width
+	local selection = nodes_where(p)
+	for i, node in ipairs(selection) do
+		a(node)
+		-- clean up, if needed (like if we just added a random offset to all nodes)
+		node.x = node.x % width
+		node.home = node.home % width
 	end
 end
 
@@ -77,9 +77,9 @@ function do_all(a)
 end
 
 function shift(d)
-	do_all(function(note)
-		note.x = note.x + d
-		note.home = note.home + d
+	do_all(function(node)
+		node.x = node.x + d
+		node.home = node.home + d
 	end)
 end
 
@@ -87,7 +87,7 @@ width = 4
 
 -- TODO: OT capture
 
--- TODO: make these overridable on a per-note basis
+-- TODO: make these overridable on a per-node basis
 d_bound = 0.5
 friction = 0
 damping = 0.005
@@ -113,7 +113,7 @@ n_keys = 128
 
 k1_held = false
 
-non_recorded_notes = Voice.new(16)
+non_recorded_nodes = Voice.new(16)
 
 function wrap_distance(a, b)
 	local d = b - a
@@ -182,19 +182,19 @@ function esq_cents(cents, velocity, length)
 	end
 end
 
-function play_note(note)
-	local pitch = note.midi_note - root_midi_note
+function play_node(node)
+	local pitch = node.midi_note - root_midi_note
 	local octave = math.floor(pitch / 12)
 	local pitch_class = pitch % 12
 	local cents = ji_cents[pitch_class + 1] + 1200 * octave
 	esq_cents(cents, 100, 1/2)
-	note.l = 1
+	node.l = 1
 end
 
-function add_note(midi_note, velocity)
+function add_node(midi_note, velocity)
 	midi_note = midi_note or 60
 	velocity = velocity or 100
-	local note = {
+	local node = {
 		x = playhead_x,
 		home = playhead_x,
 		y = y or 32,
@@ -205,28 +205,28 @@ function add_note(midi_note, velocity)
 		l = 0
 	}
 	if recording then
-		table.insert(notes, note)
-		sort_notes()
+		table.insert(nodes, node)
+		sort_nodes()
 	else
-		local slot = non_recorded_notes:get()
-		slot.note = note
+		local slot = non_recorded_nodes:get()
+		slot.node = node
 	end
-	play_note(note)
+	play_node(node)
 end
 
 function double_width()
-	-- TODO: fix the thing where a note sometimes gets wrapped or something
-	local n_notes = #notes
-	for i = 1, n_notes do
-		local note = notes[i]
-		notes[i + n_notes] = {
-			x = note.x + width,
-			home = note.home + width,
-			dx = note.dx,
-			midi_note = note.midi_note,
-			velocity = note.velocity,
+	-- TODO: fix the thing where a node sometimes gets wrapped or something
+	local n_nodes = #nodes
+	for i = 1, n_nodes do
+		local node = nodes[i]
+		nodes[i + n_nodes] = {
+			x = node.x + width,
+			home = node.home + width,
+			dx = node.dx,
+			midi_note = node.midi_note,
+			velocity = node.velocity,
 			l = 0,
-			hz = note.hz
+			hz = node.hz -- TODO: no hz
 		}
 	end
 	width = width * 2
@@ -234,18 +234,18 @@ end
 
 function halve_width()
 	local half_width = width / 2
-	-- build a new table of notes containing only those we've just heard
-	local new_notes = {}
-	for i, note in ipairs(notes) do
-		local d = wrap_distance(note.x, playhead_x)
+	-- build a new table of nodes containing only those we've just heard
+	local new_nodes = {}
+	for i, node in ipairs(nodes) do
+		local d = wrap_distance(node.x, playhead_x)
 		if d > 0 and d <= half_width then
-			if note.x >= half_width then
-				note.x = note.x - half_width
+			if node.x >= half_width then
+				node.x = node.x - half_width
 			end
-			if note.home >= half_width then
-				note.home = note.home - half_width
+			if node.home >= half_width then
+				node.home = node.home - half_width
 			end
-			table.insert(new_notes, note)
+			table.insert(new_nodes, node)
 		end
 	end
 	-- wrap play head position if needed
@@ -253,7 +253,7 @@ function halve_width()
 		playhead_x = playhead_x - half_width
 	end
 	width = width / 2
-	notes = new_notes
+	nodes = new_nodes
 end
 
 function tick()
@@ -262,16 +262,16 @@ function tick()
 		current_phase = current_phase - math.pi
 	end
 	engine.mod1(math.cos(current_phase) * 0.05 + 0.07)
-	-- move notes
-	for i, note in ipairs(notes) do
-		note.x = note.x + note.dx
-		while note.x < 0 do
-			note.x = note.x + width
+	-- move nodes
+	for i, node in ipairs(nodes) do
+		node.x = node.x + node.dx
+		while node.x < 0 do
+			node.x = node.x + width
 		end
-		while note.x >= width do
-			note.x = note.x - width
+		while node.x >= width do
+			node.x = node.x - width
 		end
-		note.l = note.l * l_decay
+		node.l = node.l * l_decay
 	end
 	-- move playhead
 	local quant = quant_ticks / ppqn
@@ -283,27 +283,27 @@ function tick()
 		quant_accumulator = (quant_accumulator + 1) % quant_ticks
 	end
 	-- update motion
-	for i, note in ipairs(notes) do
+	for i, node in ipairs(nodes) do
 		-- tend homeward
-		local ddx = homing * wrap_distance(note.x, note.home)
+		local ddx = homing * wrap_distance(node.x, node.home)
 		-- apply current
-		ddx = ddx + math.sin(current_phase + note.x / width * 2 * math.pi) * current_force
-		for j, other in ipairs(notes) do
-			if note ~= other then
-				local d = wrap_distance(note.x, other.x)
+		ddx = ddx + math.sin(current_phase + node.x / width * 2 * math.pi) * current_force
+		for j, other in ipairs(nodes) do
+			if node ~= other then
+				local d = wrap_distance(node.x, other.x)
 				local abs_d = math.abs(d)
 				-- 'd_bound' is the distance at which there is NO attraction or repulsion
-				local note_bound = d_bound * mass
-				if abs_d < 1.5 * note_bound then
+				local node_bound = d_bound * mass
+				if abs_d < 1.5 * node_bound then
 					-- repel
-					ddx = ddx + d - sign(d) * note_bound
+					ddx = ddx + d - sign(d) * node_bound
 				else
 					-- attract
-					ddx = ddx - sign(d) * math.max(0, 2 * note_bound - math.abs(d))
+					ddx = ddx - sign(d) * math.max(0, 2 * node_bound - math.abs(d))
 				end
 				-- TODO: handle note lengths too... a few options:
 				-- 1. ignore
-				-- 2. increase d_bound from centers of notes <-- this seems like the most interesting option
+				-- 2. increase d_bound from centers of nodes <-- this seems like the most interesting option
 				-- 3. get distances between starts + ends of notes and choose the smallest
 			end
 		end
@@ -311,58 +311,58 @@ function tick()
 		-- TODO: note that if mass < 0, ddx will be multiplied... which may account for some of the craziness you've observed
 		ddx = ddx / mass / inertia
 		-- 'damping' reduces speed over time, damping oscillation
-		-- when damping is 1, notes will find a comfortable spot and stay there, tending to cluster
+		-- when damping is 1, nodes will find a comfortable spot and stay there, tending to cluster
 		-- together, with tighter spacing in the center of the cluster; at 0, they'll move constantly
-		note.dx = ddx + note.dx * (1 - damping)
+		node.dx = ddx + node.dx * (1 - damping)
 		-- friction applies an opposing force, up to a limit defined by mass * friction constant
-		note.dx = sign(note.dx) * math.max(0, math.abs(note.dx) - mass * friction)
+		node.dx = sign(node.dx) * math.max(0, math.abs(node.dx) - mass * friction)
 		-- finally, clamp overall speed
-		if math.abs(note.dx) > dx_max then
-			note.dx = dx_max * sign(note.dx)
+		if math.abs(node.dx) > dx_max then
+			node.dx = dx_max * sign(node.dx)
 		end
 	end
 	if playing and quant_accumulator == 0 then
-		-- TODO: fix missed notes
-		-- detect note-playhead collisions
-		-- count down instead of up because we may end up removing elements from 'notes', which will
+		-- TODO: fix missed nodes when quant > 1
+		-- detect node-playhead collisions
+		-- count down instead of up because we may end up removing elements from 'nodes', which will
 		-- affect elements at indices > i
-		for i = #notes, 1, -1 do
-			local note = notes[i]
+		for i = #nodes, 1, -1 do
+			local node = nodes[i]
 			-- find intersection of two lines...
 			-- playhead line: x = playhead_x + quant * playing * t
-			-- note line: x = note.x + note.dx * t
-			-- playhead_x + quant * playing * t = note.x + note.dx * t
-			-- quant * playing * t - note.dx * t = note.x - playhead_x
-			-- t * (quant * playing - note.dx) = note.x - playhead_x
-			-- t = (note.x - playhead_x) / (quant - note.dx)
-			local t_collision = wrap_distance(playhead_x, note.x) / (quant * (playing and 1 or 0) - note.dx)
+			-- node line: x = node.x + node.dx * t
+			-- playhead_x + quant * playing * t = node.x + node.dx * t
+			-- quant * playing * t - node.dx * t = node.x - playhead_x
+			-- t * (quant * playing - node.dx) = node.x - playhead_x
+			-- t = (node.x - playhead_x) / (quant - node.dx)
+			local t_collision = wrap_distance(playhead_x, node.x) / (quant * (playing and 1 or 0) - node.dx)
 			if t_collision > 0 and t_collision <= 1 then
 				if erasing then
-					table.remove(notes, i)
+					table.remove(nodes, i)
 				else
 					local did_erase = false
 					if grid_erasing then
 						for k = 1, n_keys do
 							if not did_erase and keys_held[k] then
 								local held_note = get_grid_id_note(k)
-								if held_note == note.midi_note then
-									table.remove(notes, i)
+								if held_note == node.midi_note then
+									table.remove(nodes, i)
 									did_erase = true
 								end
 							end
 						end
 					end
 					if not did_erase then
-						play_note(note)
+						play_node(node)
 					end
 				end
 			end
 		end
 	end
-	-- decay non-recorded notes
-	for i, slot in ipairs(non_recorded_notes.style.slots) do
-		if slot.note ~= nil then
-			slot.note.l = slot.note.l * l_decay
+	-- decay non-recorded nodes
+	for i, slot in ipairs(non_recorded_nodes.style.slots) do
+		if slot.node ~= nil then
+			slot.node.l = slot.node.l * l_decay
 		end
 	end
 end
@@ -371,7 +371,7 @@ function midi_event(data)
 	local message = midi.to_msg(data)
 	if message.type == 'note_on' then
 		-- TODO: store note lengths
-		add_note(message.note, message.vel)
+		add_node(message.note, message.vel)
 	elseif message.type == 'stop' then
 		playing = false
 	elseif message.type == 'start' then
@@ -408,7 +408,7 @@ function grid_key(x, y, z)
 		if not grid_erasing and z == 1 then
 			-- TODO: record note ons/offs...
 			-- ...no, really
-			add_note(get_grid_note(x, y), 100)
+			add_node(get_grid_note(x, y), 100)
 		end
 	end
 end
@@ -477,26 +477,26 @@ function redraw()
 		screen.stroke()
 	end
 	screen.aa(1)
-	-- draw notes
+	-- draw nodes
 	screen.move(-127, y_center) -- TODO: ?
 	for offset = -screen_width, screen_width, screen_width do
-		for i, note in ipairs(notes) do
-			local home_x = (note.home * scale) % screen_width + 0.5 + offset
-			local x = home_x + wrap_distance(note.home, note.x) * scale
-			-- TODO: only draw this note if we can see it or it's adjacent to one we can see
-			local home_y = y_center - (note.midi_note - root_midi_note) / (3 + math.abs(wrap_distance(note.home, note.x)) * scale / 5)
-			local y = y_center - (note.midi_note - root_midi_note) / 2
+		for i, node in ipairs(nodes) do
+			local home_x = (node.home * scale) % screen_width + 0.5 + offset
+			local x = home_x + wrap_distance(node.home, node.x) * scale
+			-- TODO: only draw this node if we can see it or it's adjacent to one we can see
+			local home_y = y_center - (node.midi_note - root_midi_note) / (3 + math.abs(wrap_distance(node.home, node.x)) * scale / 5)
+			local y = y_center - (node.midi_note - root_midi_note) / 2
 			local offset = 0
 			screen.line(home_x, home_y)
 			screen.level(2)
 			screen.stroke()
 			screen.move(home_x, home_y)
 			screen.line(x, y)
-			screen.level(2 + math.floor(2 * note.l))
+			screen.level(2 + math.floor(2 * node.l))
 			screen.stroke()
-			-- draw note itself
-			screen.circle(x, y, 1.2 + 0.5 * note.l)
-			screen.level(2 + math.floor(13 * note.l))
+			-- draw node itself
+			screen.circle(x, y, 1.2 + 0.5 * node.l)
+			screen.level(2 + math.floor(13 * node.l))
 			screen.fill()
 			screen.move(home_x, home_y)
 		end
@@ -512,12 +512,12 @@ function grid_redraw()
 	g:led(1, 1, recording and 10 or 1)
 	g:led(1, 2, grid_erasing and 10 or 1)
 	note_levels = {}
-	for i, note in ipairs(notes) do
-		note_levels[note.midi_note] = (note_levels[note.midi_note] or 2) + 15 * note.l
+	for i, node in ipairs(nodes) do
+		note_levels[node.midi_note] = (note_levels[node.midi_note] or 2) + 15 * node.l
 	end
-	for i, slot in ipairs(non_recorded_notes.style.slots) do
-		if slot.note ~= nil then
-			note_levels[slot.note.midi_note] = (note_levels[slot.note.midi_note] or 0) + 15 * slot.note.l
+	for i, slot in ipairs(non_recorded_nodes.style.slots) do
+		if slot.node ~= nil then
+			note_levels[slot.node.midi_note] = (note_levels[slot.node.midi_note] or 0) + 15 * slot.node.l
 		end
 	end
 	for y = 1, g.rows do
@@ -538,7 +538,7 @@ function key(n, z)
 		erasing = z == 1
 	elseif n == 3 then
 		if z == 1 then
-			add_note()
+			add_node()
 		end
 	end
 end
